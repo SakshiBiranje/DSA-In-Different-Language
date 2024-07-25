94f954e0c3febc9cf04760bd5cb5890cc78e04c6
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-07-25 20:51:26 +0530

    Update

--- JAVA Codes/QuickSort.java
+++ JAVA Codes/QuickSort.java
@@ -1,11 +1,15 @@
 import java.util.*;
-public class QuickSort {
-    public static int partition(int array[], int low, int high) {
+public class QuickSort 
+{
+    public static int partition(int array[], int low, int high) 
+{
         int pivot = array[high];
         int i = low-1;
     
-        for(int j=low; j<high; j++){
-            if(array[j] < pivot){
+        for(int j=low; j<high; j++)
+{
+            if(array[j] < pivot)
+{
                 i++;
                 //swapping
                 int temp = array[i];
