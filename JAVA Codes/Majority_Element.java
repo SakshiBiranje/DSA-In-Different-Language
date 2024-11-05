80d35e6c456c2539e59a6de1ea281c7d46bbfb0f
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-05 11:21:26 +0530

    Update

--- JAVA Codes/Majority_Element.java
+++ JAVA Codes/Majority_Element.java
@@ -24,10 +24,13 @@ class Majority_Element{
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter the size of array elements: ");
         int size = sc.nextInt();
+
         int[] nums= new int[size];
         System.out.print("Enter the number of array elements : ");
+        
         for(int i =0;i<size;i++){
             nums[i]=sc.nextInt();
         }
