8772b0b548d7e87ef8e0cc034ef2b1e4fd6c7624
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-06 10:23:45 +0530

    update

--- JAVA Codes/BubbleSort.java
+++ JAVA Codes/BubbleSort.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class BubbleSort 
 {
-    public static void printArray(int arr[]){
-        for(int i=0 ; i<arr.length ; i++){
+    public static void printArray(int arr[])
+    {
+        for(int i=0 ; i<arr.length ; i++)
+        {
             System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int arr[]={7,8,3,1,2};
 
         //bubble sort
