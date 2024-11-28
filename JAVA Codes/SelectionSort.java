550e2985822afe93156bf66ae62c1e56e740db83
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-28 17:44:50 +0530

    Update

--- JAVA Codes/SelectionSort.java
+++ JAVA Codes/SelectionSort.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 public class SelectionSort {
-    public static void printArray(int arr[]){
+    public static void printArray(int arr[])
+    {
         for(int i=0 ; i<arr.length ; i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int arr[]={7,8,3,1,2};
 
         for(int i=0; i<arr.length-1;i++){
